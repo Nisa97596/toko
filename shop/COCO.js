55833aa413f901021bb552ff389e2e7a4f4745bc
Nisa97596 <111674074+Nisa97596@users.noreply.github.com>
2023-11-24 19:36:21 +0700

    Add files via upload

---
+++ shop/COCO.js
@@ -0,0 +1,10 @@
+// Toggle class active 
+
+const navbarNav = document.querySelector
+('.navbar-nav');
+
+// ketika menu diklik
+document.querySelector('#menu').
+onclick = ( ) => {
+    navbarNav.classList.toggle('active');
+};
